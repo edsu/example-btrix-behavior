5e9dcf42578ee85b48ae8beeb07c7e4f7e4753e9
Ed Summers <ehs@pobox.com>
2023-12-11 12:18:13 -0500

    simplified

--- run.sh
+++ run.sh
@@ -2,10 +2,4 @@
 
 docker pull webrecorder/browsertrix-crawler:latest
 
-docker run \
-    -p 9037:9037 \
-    --rm \
-    -v $PWD:/crawls/ \
-    webrecorder/browsertrix-crawler:latest crawl \
-        --config /crawls/example.yaml \
-        --customBehaviors /crawls/custom-behaviors/
+docker run -p 9037:9037 --rm -v $PWD:/crawls/ webrecorder/browsertrix-crawler:latest crawl --config /crawls/example.yaml --customBehaviors /crawls/custom-behaviors/
